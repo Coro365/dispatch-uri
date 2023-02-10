6aaf70731e2851023f266e11c80057446c086feb
Coro365 <coro.365@gmail.com>
2023-02-10 13:01:48 +0900

    feat: add write text file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate regex;
 
 use regex::Regex;
 use std::env;
+use std::fs::OpenOptions;
+use std::io::{BufWriter, Write};
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
@@ -23,12 +25,26 @@ fn main() {
 
 fn aaa(url: &str) {
     println!("aaa:\t{}", url);
+    add_uri(url)
 }
 
 fn bbb(url: &str) {
     println!("bbb:\t{}", url);
+    add_uri(url)
 }
 
 fn ccc(url: &str) {
     println!("ccc:\t{}", url);
+    add_uri(url)
+}
+
+fn add_uri(url: &str) {
+    let file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("./test.txt")
+        .expect("error");
+
+    let mut buf_writer = BufWriter::new(file);
+    writeln!(buf_writer, "{}", url).expect("error")
 }
