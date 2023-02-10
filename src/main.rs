bf35edff9b2bd6d558c62904964195e975d5f87c
Coro365 <coro.365@gmail.com>
2023-02-10 14:02:38 +0900

    feat: change to save per domain

--- src/main.rs
+++ src/main.rs
@@ -15,36 +15,31 @@ fn main() {
 
     for url in args.iter(){
         match url.as_str() {
-            n if aaa_reg.is_match(n) => aaa(n),
-            n if bbb_reg.is_match(n) => bbb(n),
-            n if ccc_reg.is_match(n) => ccc(n),
+            n if aaa_reg.is_match(n) => add_uri(n),
+            n if bbb_reg.is_match(n) => add_uri(n),
+            n if ccc_reg.is_match(n) => add_uri(n),
             n => println!("ERROR:\t{}", n),
         }
     }
 }
 
-fn aaa(url: &str) {
-    println!("aaa:\t{}", url);
-    add_uri(url)
-}
-
-fn bbb(url: &str) {
-    println!("bbb:\t{}", url);
-    add_uri(url)
-}
-
-fn ccc(url: &str) {
-    println!("ccc:\t{}", url);
-    add_uri(url)
-}
-
 fn add_uri(url: &str) {
+    println!("{}", url);
+    let filename = to_filename(url);
+
     let file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open("./test.txt")
+        .open(filename)
         .expect("error");
 
     let mut buf_writer = BufWriter::new(file);
     writeln!(buf_writer, "{}", url).expect("error")
 }
+
+fn to_filename(url: &str) -> String {
+    let filename: Vec<&str> =  url.split("/").collect();
+    let filename = filename[2].replace(".", "_");
+    return [filename, "txt".to_string()].join(".");
+}
+
